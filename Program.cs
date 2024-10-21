afa8c238b75758020b99508e5a77174b2e2d3f89
EllaNillnor <elni06001@utb.vaxjo.se>
2024-10-21 08:47:07 +0200

    .

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿{
+int tal1 = 2;
+int tal2 = 10; 
+Console.WriteLine(""+Summa(tal1,tal2)); 
+
+int Summa(int a, int b ){
+    return a+b;
+}
+}
+
+{
+int radie = 3;
+Console.WriteLine(""+AreaOmkrets(radie));
+
+    double AreaOmkrets(int r){
+        return r*2* Math.PI ;
+    }
+}
